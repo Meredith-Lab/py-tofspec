e23679c9719079519142c27978985417c53882ef
Joe Palmo <jpalmo21@amherst.edu>
2022-07-26 15:23:23 -0400

    add logo

--- docs/conf.py
+++ docs/conf.py
@@ -72,3 +72,5 @@ html_title = "py-tofspec"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_logo = "tofspec_logo.svg"
